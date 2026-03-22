e365b036cea141817b198b2db306b1cd504833fa
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2026-03-22 14:23:32 +0530

    Add files via upload

---
+++ packages/ToString.java
@@ -0,0 +1,15 @@
+public class ToString
+{
+    public void m1()
+    {
+        System.out.println();
+    }
+    public void m2()
+    {
+        System.out.println("India");
+    }
+    public String toString()
+    {
+        return "hello";
+    }
+}
---
+++ packages/ToStringDemo.java
@@ -0,0 +1,9 @@
+public class ToStringDemo 
+{
+    public static void main(String[]args)
+    
+    {
+        ToString a1 = new ToString();
+        System.out.println(a1);
+    }
+}
